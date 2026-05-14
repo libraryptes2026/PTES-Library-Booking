fc70d8f2d77d7d7c4b3d4a1810c8b4939e347ef9
libraryptes2026 <library.ptes2026@gmail.com>
2026-05-14 14:48:06 +0800

    Update lib_app.py

--- lib_app.py
+++ lib_app.py
@@ -146,8 +146,8 @@ with tab3:
                     st.error("Booking ID not found.")
 
 # --- PTES FOOTER IN SIDEBAR ---
-# st.divider()
- st.warning("⚖️ **SCHOOL HOLIDAYS: Discussion rooms reservation are between 08:00 to 11:00.** 👨‍🏫")   
+st.divider()
+st.warning("⚖️ **SCHOOL HOLIDAYS: Discussion rooms reservation are between 08:00 to 11:00.** 👨‍🏫")   
 # Custom CSS for the shapes and text
 st.markdown("""
     <style>
